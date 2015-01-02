f0b5cf94271d9ec7e79d333cb28348ed7c46fbf7
michrabi <michalrabinowitch@michals-MacBook-Pro.local>
2015-01-02 22:04:12 +0200

    Fixing reload game bug

--- Project/Assets/Scripts/Gui/startScreen.cs
+++ Project/Assets/Scripts/Gui/startScreen.cs
@@ -40,8 +40,9 @@ public class startScreen : MonoBehaviour {
 	public void RestartLevel()
 	{
 		// Restart this level
+		this.Play ();
 		gameOverSound.Stop ();
-		Application.LoadLevel(Application.loadedLevel);
+		Application.LoadLevel(1);
 	}
 	
 	public void startGame()
@@ -49,7 +50,7 @@ public class startScreen : MonoBehaviour {
 		Application.LoadLevel(1);
 		Debug.Log ("should load scene 1");
 		highScore = PlayerPrefs.GetInt(highScoreKey,0);
-		Debug.Log (highScoreKey);
+		//Debug.Log (highScoreKey);
 	}
 	
 	public void Pause () 
